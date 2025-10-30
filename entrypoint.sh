36ec680267e116b46d01b134d721194adfa19397
AlexsandroCosta <alexsandro.costasv@gmail.com>
2025-10-30 18:37:40 -0300

    comando para criação do banco de dados no container

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,13 @@ if [ "$ENVIRONMENT" = "production" ]; then
     exec poetry run gunicorn -c gunicorn.conf.py
 elif [ "$ENVIRONMENT" = "development" ]; then
     echo "Running in development mode"
+
+    echo "Aplicando migrações do banco de dados..."
+    poetry run python src/manage.py migrate
+
+    echo "Iniciando o servidor Django..."
     exec poetry run python src/manage.py runserver 0.0.0.0:8000
 else
     echo "ENVIRONMENT variable is not set"
+    exit 1
 fi
